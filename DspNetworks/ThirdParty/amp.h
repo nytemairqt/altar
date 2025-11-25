1ccfd57d6b01de95398793cc19476205deeb45fd
unknown <nytemairqt@gmail.com>
2025-11-26 08:05:55 +1000

    Moved tone stack constructors to amp instance to fix global leak issue

--- DspNetworks/ThirdParty/amp.h
+++ DspNetworks/ThirdParty/amp.h
@@ -580,6 +580,8 @@ private:
         return y;
     }    
 
+    // Tone Stack
+
     ToneStack toneStack{
         {
             PEAK(160.0f, 0.6f, low),
@@ -588,6 +590,233 @@ private:
             PEAK(6000.0f, 0.8f, presence),
         },{}        
     };    
+
+    // DC Blocker
+
+    ToneStack dcBlocker{
+        {
+            HP(140.0f),
+            HP(140.0f),
+            HP(140.0f),
+            HP(140.0f),            
+        }, {}
+    };
+
+    // CLEAN
+
+    ToneStack glass{
+        // pre
+        {
+            HP(55.0f),
+            PEAK(870.0f, 0.4f, 4.0f),
+            PEAK(3700.0f, 0.32f, 7.0f),
+            HS(6000.0f, 0.7f, 16.0f)
+        },
+        // post
+        {
+            HP(60.0f),
+            PEAK(140.0f, 2.4f, -3.5f),
+            PEAK(2200.0f, 0.3f, 4.0f),
+            PEAK(2600.0f, 5.8f, -5.1f),
+            PEAK(6400.0f, 1.0f, 4.8f),
+            LP(10000.0f)
+        }
+    };
+
+    ToneStack pearl{
+        {
+            HP(55.0f),
+            PEAK(870.0f, 0.4f, 4.0f),
+            PEAK(3700.0f, 0.32f, 7.0f),
+            HS(6000.0f, 0.7f, 16.0f)
+        },
+        {
+            HP(60.0f),
+            PEAK(140.0f, 2.4f, -3.5f),
+            PEAK(2200.0f, 0.3f, 4.0f),
+            PEAK(2600.0f, 5.8f, -5.1f),
+            PEAK(6400.0f, 1.0f, 4.8f),
+            LP(10000.0f)
+        }
+    };
+
+    ToneStack diamond{
+        {
+            HP(55.0f),
+            PEAK(870.0f, 0.4f, 4.0f),
+            PEAK(3700.0f, 0.32f, 7.0f),
+            HS(6000.0f, 0.7f, 16.0f)
+        },
+        {
+            HP(60.0f),
+            PEAK(140.0f, 2.4f, -3.5f),
+            PEAK(2200.0f, 0.3f, 4.0f),
+            PEAK(2600.0f, 5.8f, -5.1f),
+            PEAK(6400.0f, 1.0f, 4.8f),
+            LP(10000.0f)
+        }
+    };
+
+    ToneStack zircon{
+        {
+            HP(55.0f),
+            PEAK(870.0f, 0.4f, 4.0f),
+            PEAK(3700.0f, 0.32f, 7.0f),
+            HS(6000.0f, 0.7f, 16.0f)
+        },
+        {
+            HP(60.0f),
+            PEAK(140.0f, 2.4f, -3.5f),
+            PEAK(2200.0f, 0.3f, 4.0f),
+            PEAK(2600.0f, 5.8f, -5.1f),
+            PEAK(6400.0f, 1.0f, 4.8f),
+            LP(10000.0f)
+        }
+    };
+
+    // DIRTY
+
+    ToneStack slate{
+       {
+            HP(82.0f),
+            PEAK(650.0f, 1.2f, 2.5f),
+            PEAK(2200.0f, 0.8f, 3.2f),
+            HS(4500.0f, 0.7f, 6.0f)
+       },
+       {
+            HP(40.0f),
+            LS(45.0f, 0.5f, 6.0f),
+            PEAK(75.1f, 0.46f, 4.38f),
+            PEAK(120.0f, 0.5f, 8.0f),
+            LS(150.0f, 0.7f, -2.0f),
+            PEAK(160.0f, 0.6f, 2.0f),
+            PEAK(750.0f, 1.4f, 4.0f),
+            PEAK(1800.0f, 1.0f, 2.8f),
+            PEAK(2500.0f, 2.11f, 1.86f),
+            PEAK(3200.0f, 1.2f, -1.5f),
+            PEAK(3638.0f, 1.156f, 4.616f),
+            PEAK(3670, 1.14f, 3.48f),
+            PEAK(5000.0f, 0.8f, 2.0f),
+            HS(6500.0f, 0.7f, -1.8f),
+            PEAK(7842.0f, 0.938f, 4.032f),
+            HS(9000.0f, 0.6f, 3.5f),
+            LP(12000.0f),
+       }
+    };
+
+    ToneStack obsidian{
+        {
+            HP(60.0f),
+            PEAK(520.0f, 0.9f, 2.8f),
+            PEAK(1400.0f, 0.7f, 3.2f),
+            PEAK(3800.0f, 1.1f, 4.5f),
+        },
+        {
+            HP(32.0f),
+            HP(32.0f),
+            RBJ_LS(120.0f, 0.5f, 11.712f),
+            PEAK(129.0f, 1.74f, 5.86f),
+            PEAK(180.0f, 0.813f, 3.0f),
+            PEAK(528.0f, 0.71f, -9.0f),
+            PEAK(600.0f, 0.8f, 3.84f),
+            PEAK(1100.0f, 1.374f, -5.872f),
+            PEAK(1787.0f, 0.813, -2.496f),
+            PEAK(2185.0f, 0.437f, 7.686f),
+            PEAK(2900.0f, 2.03f, 3.60f),
+            HS(6000.0f, 0.8f, 5.0f),
+            HS(7760.0f, 0.813f, 2.5f),
+            PEAK(8678.0f, 0.688f, 3.532f),
+            LP(17122.0f)
+        }
+    };
+
+    ToneStack amethyst{
+        {
+            HP(60.0f),
+            PEAK(500.0f, 1.1f, 2.8f),
+            PEAK(1600.0f, 0.9f, 3.5f),
+            PEAK(4000.0f, 1.3f, 4.0f),
+        },
+        {
+            HP(45.0f),
+            LS(45.0f, 0.5f, 6.0f),
+            PEAK(120.0f, 0.5f, 8.0f),
+            PEAK(180.0f, 1.2f, -1.8f),
+            PEAK(600.0f, 1.0f, 3.2f),
+            PEAK(1200.0f, 0.8f, 2.5f),
+            PEAK(2800.0f, 1.4f, -2.0f),
+            PEAK(4500.0f, 1.1f, 4.5f),
+            PEAK(6500.0f, 0.9f, 2.8f),
+            HS(8500.0f, 0.7f, 3.5f),
+            LP(13000.0f)
+        }
+    };
+
+    ToneStack opal{
+        {
+            HP(90.0f),
+            PEAK(720.0f, 1.5f, 3.8f),
+            PEAK(2000.0f, 1.0f, 2.5f),
+            PEAK(5500.0f, 1.8f, 5.2f)
+        },
+        {
+            HP(40.0f),
+            HP(45.0f),
+            LS(45.0f, 0.5f, 6.0f),
+            PEAK(120.0f, 0.5f, 8.0f),
+            PEAK(200.0f, 1.5f, -3.2f),
+            PEAK(800.0f, 1.2f, 4.5f),
+            PEAK(1600.0f, 0.9f, 2.8f),
+            PEAK(3200.0f, 2.0f, -2.8f),
+            PEAK(5800.0f, 1.3f, 4.8f),
+            HS(7500.0f, 0.8f, -2.5f),
+            LP(11500.0f)
+        }
+    };
+
+    ToneStack sapphire{
+        {
+            HP(85.0f),
+            PEAK(680.0f, 1.4f, 4.2f),
+            PEAK(1800.0f, 0.9f, 3.5f),
+            PEAK(4800.0f, 1.6f, 5.8f),
+        },
+        {
+            HP(45.0f),
+            LS(45.0f, 0.5f, 6.0f),
+            PEAK(120.0f, 0.5f, 8.0f),
+            PEAK(150.0f, 1.2f, -2.5f),
+            PEAK(750.0f, 1.1f, 4.8f),
+            PEAK(1500.0f, 0.8f, 2.8f),
+            PEAK(2800.0f, 1.8f, -2.2f),
+            PEAK(5200.0f, 1.2f, 5.5f),
+            PEAK(7200.0f, 0.9f, 3.2f),
+            HS(9000.0f, 0.7f, -1.8f),
+            LP(12800.0f)
+        }   
+    };
+
+    ToneStack garnet{
+        {
+            HP(50.0f),
+            PEAK(320.0f, 0.8f, 2.2f),
+            PEAK(1000.0f, 0.7f, 2.8f),
+            PEAK(2800.0f, 0.6f, 3.8f),
+        },
+        {
+            HP(45.0f),
+            LS(45.0f, 0.5f, 6.0f),
+            LS(100.0f, 0.7f, 2.0f),
+            PEAK(120.0f, 0.5f, 8.0f),
+            PEAK(360.0f, 0.9f, 2.5f),
+            PEAK(850.0f, 0.8f, 2.8f),
+            PEAK(1600.0f, 0.9f, 2.2f),
+            PEAK(3200.0f, 1.1f, 3.5f),
+            PEAK(5500.0f, 0.8f, 3.8f),
+            HS(7500.0f, 0.7f, 4.5f),
+            LP(16000.0f)
+        }
+    };
 
     void updateOversampling()
     {
